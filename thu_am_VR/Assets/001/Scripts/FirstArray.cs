923e694299d3b0b6b6f9b6f5e6a7ee114b872825
kdduu <jyw030729@gmail.com>
2023-03-09 11:57:07 +0900

    첫번째 배열 테스트

--- thu_am_VR/Assets/001/Scripts/FirstArray.cs
+++ thu_am_VR/Assets/001/Scripts/FirstArray.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class FirstArray : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int[] number = new int[] { 9, -11, 6, -12, 1 };
     void Start()
     {
-        
+        number[1] = 11;
+        number[2] = 12;
+        Debug.Log(number[0]);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
